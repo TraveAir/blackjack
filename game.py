08c8fc27f72b7068e049be04d0132c7ed1adddc1
Travis M <coding@therealtravis.com>
2023-05-10 00:39:47 -0700

    adding functionality

--- game.py
+++ game.py
@@ -6,9 +6,26 @@ Created on Tue May 9, 2023
 import random
 import time
 
-NUM_DECKS = 1
-SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
-NAMES = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+NUM_DECKS = 6
+
+STARTING_BALANCE = 1000
+
+CARD_SUITS = ["♥", "♦", "♣", "♠"]
+CARD_NAMES = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+]
 
 
 class Card:
@@ -16,9 +33,10 @@ class Card:
         self.name = name
         self.suit = suit
         self.value = value
+        self.hidden = False
 
     def __repr__(self):
-        return f"{self.name} of {self.suit} with value {self.value}"
+        return f"[{self.name} of {self.suit}] "
 
 
 class Shoe:
@@ -29,8 +47,8 @@ class Shoe:
 
     def build(self):
         for _ in range(NUM_DECKS):
-            for suit in SUITS:
-                for index, name in enumerate(NAMES, start=1):
+            for suit in CARD_SUITS:
+                for index, name in enumerate(CARD_NAMES, start=1):
                     value = index
                     if name in ["Jack", "Queen", "King"]:
                         value = 10
@@ -57,17 +75,79 @@ class Shoe:
 
 
 class Hand:
-    cards = []
-    bet_amount = 0
+    def __init__(self):
+        self.cards = []
+        self.bet_amount = 0
+        self.busted = False
+
+    def __repr__(self) -> str:
+        ret = ""
+        for card in self.cards:
+            ret += f"{card}"
+        return ret
+
+    def display(self):
+        card_size = 9
+        hline = ""
+        vline = "|"
+        vspacer = ""
+        hspacer = ""
+        s = ""
+
+        for _ in range(card_size):
+            hline = f"{hline}-"
+            vline = f"{vline} "
+        vline = f"{vline}|"
+
+        for card in self.cards:
+            vspacer = f"{vspacer}\t{vline}"
+            hspacer = f"{hspacer}\t {hline}"
+
+        print(hspacer)
+        print(vspacer)
+
+        for card in self.cards:
+            if card.name == "10":
+                s = f"{s}\t{vline[:2]}{card.name}{vline[4:]}"
+            else:
+                s = f"{s}\t{vline[:2]}{card.name[0]}{vline[3:]}"
+        print(s)
+        s = ""
+
+        print(vspacer)
+
+        for card in self.cards:
+            index = len(vline) // 2
+            s = f"{s}\t{vline[:index]}{card.suit}{vline[index+1:]}"
+        print(s)
+        s = ""
+
+        print(vspacer)
+
+        for card in self.cards:
+            index = len(vline) - 3
+            if card.name == "10":
+                s = f"{s}\t{vline[:index-1]}{card.name}{vline[index+1:]}"
+            else:
+                s = f"{s}\t{vline[:index]}{card.name[0]}{vline[index+1:]}"
+        print(s)
+        s = ""
+
+        print(vspacer)
+        print(hspacer)
 
 
 class Player:
-    balance = 0
-    hands = []
+    def __init__(self):
+        self.hands = [Hand()]
+        self.balance = STARTING_BALANCE
+        self.streak = 0
 
 
 shoe = Shoe()
-shoe.shuffle()
-shoe.display()
-shoe.clear()
-shoe.deal()
+player = Player()
+player.hands[0].cards.append(shoe.deal())
+player.hands[0].cards.append(shoe.deal())
+player.hands[0].cards.append(shoe.deal())
+
+player.hands[0].display()
